b71e16d51109e348c878f79fdc5c057312461b8e
Matvii Chepur <matvii.chepur@shelterzoom.com>
2024-08-01 11:49:11 +0300

    'Tests for todomvc are fixed, size check is added'

--- tests/todomvc.spec.js
+++ tests/todomvc.spec.js
@@ -1,20 +1,19 @@
 import { browser, by, be, have, command } from 'selenidejs';
 import { test } from '@jest/globals';
 
-test('elements a, b, c should be added to list, and b should be completed', async () => {
-    await browser.open('https://todomvc-emberjs-app.autotest.how/');
+test('should successfully mark as completed b todo in the list', async () => {
+  await browser.open('https://todomvc-emberjs-app.autotest.how/')
     
-    await browser.element('#new-todo').type('a').then(command.pressEnter);
-    await browser.element('#new-todo').type('b').then(command.pressEnter);
-    await browser.element('#new-todo').type('c').then(command.pressEnter);
+  await browser.element('#new-todo').type('a').then(command.pressEnter)
+  await browser.element('#new-todo').type('b').then(command.pressEnter)
+  await browser.element('#new-todo').type('c').then(command.pressEnter)
 
-    await browser.all('#todo-list>li').should(have.exactTexts('a', 'b', 'c'));
+  await browser.all('#todo-list>li').should(have.exactTexts('a', 'b', 'c'))
 
-    await browser.all('#todo-list>li').elementBy(have.exactText('b'))
-        .element('.toggle').click();
+  await browser.element('#todo-list>li:nth-child(2).toggle').click()
 
-    await browser.all('#todo-list>li').by(have.cssClass('completed'))
-        .should(have.exactTexts('b'));
-    await browser.all('#todo-list>li').by(have.no.cssClass('completed'))
-        .should(have.exactTexts('a', 'c'));
+  await browser.all('#todo-list>li.completed').should(have.exactTexts('b'))
+  await browser.all('#todo-list>li:not(.completed)').should(have.exactTexts('a', 'c'))
+  await browser.all('#todo-list>li').should(have.exactTexts('a', 'c'))
+  await browser.all('#todo-list>li').should(have.size(3))
 });
