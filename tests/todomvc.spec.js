9afdcaec2cab087389b5edb82a6f59b60641c695
Matvii Chepur <5mystogan5@gmail.com>
2024-08-02 10:52:59 +0300

    Tests are refactored

--- tests/todomvc.spec.js
+++ tests/todomvc.spec.js
@@ -1,18 +1,20 @@
-import { browser, by, be, have, command } from 'selenidejs';
-import { test } from '@jest/globals';
+import { browser, by, be, have, command } from 'selenidejs'
+import { test } from '@jest/globals'
 
-test('should successfully mark as completed b todo in the list', async () => {
+test('todos is mark as completed', async () => {
   await browser.open('https://todomvc-emberjs-app.autotest.how/')
-    
+
+  // WHEN
   await browser.element('#new-todo').type('a').then(command.pressEnter)
   await browser.element('#new-todo').type('b').then(command.pressEnter)
   await browser.element('#new-todo').type('c').then(command.pressEnter)
 
   await browser.all('#todo-list>li').should(have.exactTexts('a', 'b', 'c'))
 
-  await browser.element('#todo-list>li:nth-child(2)>div>.toggle').click()
+  // WHEN
+  await browser.element('.todo-list li:nth-of-type(2) .toggle').click()
 
   await browser.all('#todo-list>li.completed').should(have.exactTexts('b'))
   await browser.all('#todo-list>li:not(.completed)').should(have.exactTexts('a', 'c'))
-  await browser.all('#todo-list>li').should(have.size(3))
+  await browser.all('#todo-list>li').should(have.texts('a', 'b', 'c'))
 });
