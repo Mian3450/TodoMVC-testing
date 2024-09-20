63b1b3ed273105318e8c01e529c844d663a2a405
WSL Matvii Chepur <5mystogan5@gmail.com>
2024-09-20 14:07:58 +0300

    XPath problems fixed

--- tests/xpath-version/todomvc.spec.js
+++ tests/xpath-version/todomvc.spec.js
@@ -1,21 +1,21 @@
-import { browser, by, be, have, command } from 'selenidejs'
+import { browser, have, command } from 'selenidejs'
 import { test } from '@jest/globals'
 
 test('todo is mark as completed', async () => {
   // GIVEN
   await browser.open('https://todomvc-emberjs-app.autotest.how/')
 
-  await browser.element('//input[@id="new-todo"]').type('a').then(command.pressEnter)
-  await browser.element('//input[@id="new-todo"]').type('b').then(command.pressEnter)
-  await browser.element('//input[@id="new-todo"]').type('c').then(command.pressEnter)
+  await browser.element('//*[contains(@id, "new-todo")]').type('a').then(command.pressEnter)
+  await browser.element('//*[contains(@id, "new-todo")]').type('b').then(command.pressEnter)
+  await browser.element('//*[contains(@id, "new-todo")]').type('c').then(command.pressEnter)
 
-  await browser.all('//ul[@id="todo-list"]/li').should(have.exactTexts('a', 'b', 'c'))
+  await browser.all('//*[contains(@id, "todo-list")]/li').should(have.exactTexts('a', 'b', 'c'))
 
   // WHEN
-  await browser.element('//ul[@id="todo-list"]/li[2]//input[@class="toggle"]').click()
+  await browser.element('//*[contains(@id, "todo-list")]/li[2]//*[@class="toggle"]').click()
 
   // THEN
-  await browser.all('//ul[@id="todo-list"]/li[contains(@class, "completed")]').should(have.exactTexts('b'))
-  await browser.all('//ul[@id="todo-list"]/li[not(contains(@class, "completed"))]').should(have.exactTexts('a', 'c'))
-  await browser.all('//ul[@id="todo-list"]/li').should(have.texts('a', 'b', 'c'))
+  await browser.all('//*[contains(@id, "todo-list")]/li[contains(@class, "completed")]').should(have.exactTexts('b'))
+  await browser.all('//*[contains(@id, "todo-list")]/li[not(contains(@class, "completed"))]').should(have.exactTexts('a', 'c'))
+  await browser.all('//*[contains(@id, "todo-list")]/li').should(have.texts('a', 'b', 'c'))
 })
