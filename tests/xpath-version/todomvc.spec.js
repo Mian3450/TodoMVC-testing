88a609126e16b4ba2a1cb1e281081640d7bebb96
WSL Matvii Chepur <5mystogan5@gmail.com>
2025-01-13 21:11:15 +0200

    Format of code fixed | .gitignore for Prettier updated

--- tests/xpath-version/todomvc.spec.js
+++ tests/xpath-version/todomvc.spec.js
@@ -1,12 +1,12 @@
-import { browser, have, command } from "selenidejs"
-import { test } from "@jest/globals"
+import { browser, have, command } from 'selenidejs'
+import { test } from '@jest/globals'
 
-test("todo is mark as completed", async () => {
+test('todo is mark as completed', async () => {
   // GIVEN
-  await browser.open("https://todomvc-emberjs-app.autotest.how/")
-  await browser.element('//*[@id="new-todo"]').type("a").then(command.pressEnter)
-  await browser.element('//*[@id="new-todo"]').type("b").then(command.pressEnter)
-  await browser.element('//*[@id="new-todo"]').type("c").then(command.pressEnter)
+  await browser.open('https://todomvc-emberjs-app.autotest.how/')
+  await browser.element('//*[@id="new-todo"]').type('a').then(command.pressEnter)
+  await browser.element('//*[@id="new-todo"]').type('b').then(command.pressEnter)
+  await browser.element('//*[@id="new-todo"]').type('c').then(command.pressEnter)
 
   // WHEN
   await browser.element(
@@ -14,11 +14,11 @@ test("todo is mark as completed", async () => {
     ).click()
 
   // THEN
-  await browser.all(
-      '//*[@id="todo-list"]/li[contains(concat(" ", normalize-space(@class), " "), " completed ")]'
-    ).should(have.exactTexts("b"))
-  await browser.all(
-      '//*[@id="todo-list"]/li[not(contains(concat(" ", normalize-space(@class), " "), " completed "))]'
-    ).should(have.exactTexts("a", "c"))
-  await browser.all('//*[@id="todo-list"]/li').should(have.texts("a", "b", "c"))
-});
+  await browser
+    .all('//*[@id="todo-list"]/li[contains(concat(" ", normalize-space(@class), " "), " completed ")]')
+    .should(have.exactTexts('b'))
+  await browser
+    .all('//*[@id="todo-list"]/li[not(contains(concat(" ", normalize-space(@class), " "), " completed "))]')
+    .should(have.exactTexts('a', 'c'))
+  await browser.all('//*[@id="todo-list"]/li').should(have.texts('a', 'b', 'c'))
+})
