ca81702aa651de0c841ac012d03f971abc230387
WSL Matvii Chepur <5mystogan5@gmail.com>
2025-06-09 22:02:12 +0300

    Code format fixed | Structure of utils updated | Parallel using of XPathBuilder handled

--- tests/xpath-version/todomvc.spec.js
+++ tests/xpath-version/todomvc.spec.js
@@ -1,6 +1,7 @@
 import { browser, have, command } from 'selenidejs'
 import { test } from '@jest/globals'
-import { x, its } from '../../lib/xpath.js'
+import { x } from '../../lib/xpath-dsl/xpath-builder.js'
+import { its } from '../../lib/xpath-dsl/predicates.js'
 
 test('todo is mark as completed', async () => {
   // GIVEN
@@ -11,36 +12,20 @@ test('todo is mark as completed', async () => {
 
   // WHEN
   await browser.element(
-    x.all()
-      .by(its.id('todo-list'))
-      .child('li')
-      .by(its.descendantWithText('b'))
-      .descendant()
-      .by(its.cssClass('toggle'))
-      .x
+    x.all().by(its.id('todo-list')).child('li').by(its.descendantWithText('b'))
+      .descendant().by(its.cssClass('toggle')).x
   ).click()
 
   // THEN
   await browser.all(
-    x.all()
-      .by(its.id('todo-list'))
-      .child('li')
-      .by(its.cssClass('completed'))
-      .x
+    x.all().by(its.id('todo-list')).child('li').by(its.cssClass('completed')).x
   ).should(have.exactTexts('b'))
 
   await browser.all(
-    x.all()
-      .by(its.id('todo-list'))
-      .child('li')
-      .by(`not(${its.cssClass('completed')})`)
-      .x
+    x.all().by(its.id('todo-list')).child('li').by(`not(${its.cssClass('completed')})`).x
   ).should(have.exactTexts('a', 'c'))
 
   await browser.all(
-    x.all()
-      .by(its.id('todo-list'))
-      .child('li')
-      .x
+    x.all().by(its.id('todo-list')).child('li').x
   ).should(have.texts('a', 'b', 'c'))
 })
